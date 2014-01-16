3afc4dcadfaef8b0b333bbe05a6cabb4b40969d1
vinsonlee <vinsonlee@users.noreply.github.com>
2014-01-15 23:35:30 -0800

    crypto/ps2: WIP

---
+++ crypto-009/ps2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# Question 1
+# CBC
+key1 = "140b41b22a29beb4061bda66b6747e14"
+ciphertext1 = "4ca00ff4c898d61e1edbf1800618fb2828a226d160dad07883d04e008a7897ee2e4b7465d5290d0c0e6c6822236e1daafb94ffe0c5da05d9476be028ad7c1d81"
+
+# Question 2
+# CBC
+key2 = "140b41b22a29beb4061bda66b6747e14"
+ciphertext2 = "5b68629feb8606f9a6667670b75b38a5b4832d0f26e1ab7da33249de7d4afc48e713ac646ace36e872ad5fb8a512428a6e21364b0c374df45503473c5242a253"
+
+# Question 3
+# CTR
+key3 = "36f18357be4dbd77f050515c73fcf9f2"
+ciphertext3 = "69dda8455c7dd4254bf353b773304eec0ec7702330098ce7f7520d1cbbb20fc388d1b0adb5054dbd7370849dbf0b88d393f252e764f1f5f7ad97ef79d59ce29f5f51eeca32eabedd9afa9329"
+
+# Question 4
+key4 = "36f18357be4dbd77f050515c73fcf9f2"
+ciphertext4 = "770b80259ec33beb2561358a9f2dc617e46218c0a53cbeca695ae45faa8952aa0e311bde9d4e01726d3184c34451"
+
+
+print len(key1)
+print len(ciphertext1)
+
+print len(key2)
+print len(ciphertext2)
+
+print len(key3)
+print len(ciphertext3)
+
+print len(key4)
+print len(ciphertext4)
