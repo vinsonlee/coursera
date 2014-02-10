d363a2f745b672a90924b2f6275dd691f55c74b9
vinsonlee <vinsonlee@users.noreply.github.com>
2014-02-09 23:49:36 -0800

    algs4partI-004: queues WIP
    Compilation:  PASSED
    Style:        PASSED
    Findbugs:     No potential bugs found.
    API:          PASSED
    
    Correctness:  32/32 tests passed
    Memory:       47/47 tests passed
    Timing:       24/24 tests passed
    
    Raw score: 100.00% [Correctness: 65%, Memory: 10%, Timing: 25%, Style: 0%]

--- algs4partI-004/Subset.java
+++ algs4partI-004/Subset.java
@@ -1,4 +1,15 @@
 public class Subset {
     public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> q = new RandomizedQueue<String>();
+
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            q.enqueue(item);
+        }
+
+        for (int i = 0; i < k; i++) {
+            StdOut.println(q.dequeue());
+        }
     }
 }
