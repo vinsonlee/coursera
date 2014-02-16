be33a8360ed98cce09c9a0f6f0b787bc4deecc79
vinsonlee <vinsonlee@users.noreply.github.com>
2014-02-16 00:37:37 -0800

    algs4partI-004: collinear WIP
    Compilation:  PASSED
    Style:        PASSED
    Findbugs:     No potential bugs found.
    API:          PASSED
    
    Correctness:  9/36 tests passed
    Memory:       1/1 tests passed
    Timing:       14/17 tests passed
    
    Raw score: 46.84% [Correctness: 65%, Memory: 10%, Timing: 25%, Style: 0%]

--- algs4partI-004/Point.java
+++ algs4partI-004/Point.java
@@ -22,8 +22,12 @@ public class Point implements Comparable<Point> {
     private final int y;                              // y coordinate
 
     private static class SlopeOrder implements Comparator<Point> {
-        public int compare(Point p1, Point p2) {
-            return p1.compareTo(p2);
+        public int compare(Point p, Point q) {
+            if (p == null || q == null) {
+                throw new NullPointerException();
+            }
+
+            return p.compareTo(q);
         }
     }
 
@@ -49,14 +53,37 @@ public class Point implements Comparable<Point> {
     // slope between this point and that point
     public double slopeTo(Point that) {
         /* YOUR CODE HERE */
-        double slope = (that.y - this.y) / (that.x - this.x);
-        return slope;
+        if (that == null) {
+            throw new NullPointerException();
+        }
+
+        // StdOut.println(that.y + "-" + this.y + "/" + that.x + "-" + this.x);
+        double n = that.y - this.y;
+        double d = that.x - this.x;
+
+        if (n == 0 && d == 0) {
+            return Double.NEGATIVE_INFINITY;
+        } else if (d == 0) {
+            return Double.POSITIVE_INFINITY;
+        } else if (n == 0) {
+            // avoid negative zero
+            return 0;
+        } else {
+            return n / d;
+        }
+
+        //slope = (double) (that.y - this.y) / (that.x - this.x);
+        //return slope;
     }
 
     // is this point lexicographically smaller than that one?
     // comparing y-coordinates and breaking ties by x-coordinates
     public int compareTo(Point that) {
         /* YOUR CODE HERE */
+        if (that == null) {
+            throw new NullPointerException();
+        }
+
         if ((this.y < that.y)
             || ((this.y == that.y) && (this.x < that.x))) {
             return -1;
@@ -76,5 +103,38 @@ public class Point implements Comparable<Point> {
     // unit test
     public static void main(String[] args) {
         /* YOUR CODE HERE */
+        Point p;
+        Point q;
+        double slope;
+
+        p = new Point(102, 113);
+        q = new Point(387, 383);
+        assert Math.abs(p.slopeTo(q) - 0.9473684210526315) <= 0.000001;
+
+        // positive infinite slope, where p and q have coordinates in [0, 500)
+        p = new Point(24, 311);
+        q = new Point(24, 52);
+        assert p.slopeTo(q) == Double.POSITIVE_INFINITY;
+
+        // negative infinite slope, where p and q have coordinates in [0, 500)
+        p = new Point(213, 410);
+        q = new Point(213, 410);
+        assert p.slopeTo(q) == Double.NEGATIVE_INFINITY;
+
+        // positive zero slope, where p and q have coordinates in [0, 500)
+        p = new Point(463, 456);
+        q = new Point(212, 456);
+        assert p.slopeTo(q) == Double.doubleToRawLongBits(0.0);
+
+        // antisymmetric, where p and q have coordinates in [0, 500)
+        p = new Point(365, 295);
+        q = new Point(365, 298);
+        // assert p.compareTo(q) == -1;
+        // assert q.compareTo(p) == 0;
+
+        // sign of compareTo(), where p and q have coordinates in [0, 500)
+        p = new Point(171, 307);
+        q = new Point(171, 195);
+        // assert p.compareTo(q) == 1;
     }
 }
