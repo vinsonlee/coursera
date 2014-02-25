3d8a995b6065af42b5812a4a13873896cffd1be3
vinsonlee <vinsonlee@users.noreply.github.com>
2014-02-24 16:46:21 -0800

    algs4partI-004: 8puzzle WIP
    Compilation:  PASSED
    Style:        PASSED
    Findbugs:     No potential bugs found.
    API:          PASSED
    
    Correctness:  16/25 tests passed
    Memory:       8/8 tests passed
    Timing:       17/17 tests passed
    
    Raw score: 76.60% [Correctness: 65%, Memory: 10%, Timing: 25%, Style: 0%]

--- algs4partI-004/Solver.java
+++ algs4partI-004/Solver.java
@@ -1,21 +1,103 @@
 public class Solver {
+    private boolean solvable;
+    private MinPQ<Node> pq;
+    private Queue<Board> solution;
+    private int moves;
+
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
+        solvable = false;
+        pq = new MinPQ<Node>();
+        solution = new Queue<Board>();
+        moves = 0;
+
+        Node initialNode = new Node();
+        initialNode.board = initial;
+        initialNode.moves = 0;
+        initialNode.prev = null;
+
+        pq.insert(initialNode);
+
+        int step = 0;
+
+        while (true) {
+            /*
+            StdOut.println("Step: " + step);
+            for (Node n : pq) {
+                StdOut.println(n);
+            }
+             */
+            step++;
+
+            Node node = pq.delMin();
+            solution.enqueue(node.board);
+
+            // check node isGoal
+            if (node.board.isGoal()) {
+                solvable = true;
+                moves = node.moves;
+                break;
+            } else {
+                for (Board board : node.board.neighbors()) {
+                    Node neighborNode = new Node();
+                    neighborNode.board = board;
+                    neighborNode.moves = node.moves + 1;
+                    neighborNode.prev = node;
+
+                    if (node.prev == null
+                        || !neighborNode.board.equals(node.prev.board)) {
+                        pq.insert(neighborNode);
+                    }
+                }
+            }
+        }
     }
 
     // is the initial board solvable?
     public boolean isSolvable() {
-        return false;
+        return solvable;
     }
 
     // min number of moves to solve initial board; -1 if no solution
     public int moves() {
-        return 0;
+        return moves;
     }
 
     // sequence of boards in a shortest solution; null if no solution
     public Iterable<Board> solution() {
-        return null;
+        return solution;
+    }
+
+    private class Node implements Comparable<Node> {
+        private Board board;
+        private int moves;
+        private Node prev;
+
+        public int compareTo(Node that) {
+            return this.priority() - that.priority();
+
+            /*
+            if (this.priority() != that.priority()) {
+                return this.priority() - that.priority();
+            } else {
+                return this.moves - that.moves;
+            }
+             */
+        }
+
+        public int priority() {
+            return moves + board.manhattan();
+        }
+
+        public String toString() {
+            String s = "";
+            s += "priority  = " + priority() + "\n";
+            s += "moves     = " + moves + "\n";
+            s += "manhattan = " + board.manhattan() + "\n";
+            s += board;
+
+            return s.toString();
+        }
     }
 
     // solve a slider puzzle (given below)
