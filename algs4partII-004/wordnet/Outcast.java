e2e5f13aa62198c9b18a322a52ad6cd3fe8b4144
vinsonlee <vinsonlee@users.noreply.github.com>
2014-11-09 12:47:14 -0800

    algs4partII-004/wordnet: WIP
    Compilation:  PASSED
    Style:        PASSED
    Findbugs:     Potential bugs found.
    API:          PASSED
    
    Correctness:  34/34 tests passed
    Memory:       2/2 tests passed
    Timing:       9/10 tests passed
    
    Aggregate score: 97.50% [Correctness: 65%, Memory: 10%, Timing: 25%, Style: 0%]

--- algs4partII-004/wordnet/Outcast.java
+++ algs4partII-004/wordnet/Outcast.java
@@ -1,13 +1,37 @@
 public class Outcast {
+    private WordNet wordnet;
+
     // constructor takes a WordNet object
     public Outcast(WordNet wordnet) {
-        assert false;
+        this.wordnet = wordnet;
     }
 
     // given an array of WordNet nouns, return an outcast
     public String outcast(String[] nouns) {
-        assert false;
-        return null;
+        int distance = 0;
+        String outcast = null;
+
+        for (String i : nouns) {
+            int d = 0;
+
+            for (String j : nouns) {
+                int dist = wordnet.distance(i, j);
+                // StdOut.println("distance(" + i + "," + j + ") = " + dist);
+                d += dist;
+            }
+
+            // StdOut.println("distance: " + d);
+
+            if (d > distance) {
+                distance = d;
+                outcast = i;
+            }
+
+            // StdOut.println("distance: " + d + " outcast: " + outcast);
+        }
+
+        assert outcast != null;
+        return outcast;
     }
 
     // see test client below
