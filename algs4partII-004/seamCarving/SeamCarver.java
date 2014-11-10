0704b627a5291f9cd93c9ecfc6d8e9dbb8ff082d
vinsonlee <vinsonlee@users.noreply.github.com>
2014-11-09 18:34:45 -0800

    algs4partII-004/seamCarving: Add initial files.
    Compilation:  PASSED
    Style:        PASSED
    Findbugs:     No potential bugs found.
    API:          PASSED
    
    Correctness:  0/18 tests passed
    Memory:       7/7 tests passed
    Timing:       6/6 tests passed
    
    Aggregate score: 35.00% [Correctness: 65%, Memory: 10%, Timing: 25%, Style: 0%]

---
+++ algs4partII-004/seamCarving/SeamCarver.java
@@ -0,0 +1,43 @@
+public class SeamCarver {
+    // create a seam carver object based on the given picture
+    public SeamCarver(Picture picture) {
+    }
+
+    // current picture
+    public Picture picture() {
+        return null;
+    }
+
+    // width of current picture
+    public int width() {
+        return 0;
+    }
+
+    // height of current picture
+    public int height() {
+        return 0;
+    }
+
+    /// energy of pixel at column x and row y
+    public double energy(int x, int y) {
+        return 0;
+    }
+
+    // sequence of indices for horizontal seam
+    public int[] findHorizontalSeam() {
+        return null;
+    }
+
+    // sequence of indices for vertical seam
+    public int[] findVerticalSeam() {
+        return null;
+    }
+
+    // remove horizontal seam from current picture
+    public void removeHorizontalSeam(int[] seam) {
+    }
+
+    // remove vertical seam from current picture
+    public void removeVerticalSeam(int[] seam) {
+    }
+}
