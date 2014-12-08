51b32432ef8ccfa18126dc8fa5b5b6bc08230a81
vinsonlee <vinsonlee@users.noreply.github.com>
2014-12-08 10:35:04 -0800

    algs4partII-004/boggle: WIP
    Compilation:  PASSED
    Style:        PASSED
    Findbugs:     No potential bugs found.
    API:          PASSED
    
    Correctness:  2/13 tests passed
    Memory:       3/3 tests passed
    Timing:       6/6 tests passed
    
    Aggregate score: 45.00% [Correctness: 65%, Memory: 10%, Timing: 25%, Style: 0%]

--- algs4partII-004/boggle/BoggleSolver.java
+++ algs4partII-004/boggle/BoggleSolver.java
@@ -1,19 +1,47 @@
 public class BoggleSolver
 {
+    private SET<String> dictionary;
+
     // Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
     public BoggleSolver(String[] dictionary) {
+        this.dictionary = new SET<String>();
+        for (String s : dictionary) {
+            this.dictionary.add(s);
+        }
     }
 
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
     public Iterable<String> getAllValidWords(BoggleBoard board) {
-        return null;
+        SET<String> vaildWords = new SET<String>();
+
+        return vaildWords;
     }
 
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
     public int scoreOf(String word) {
-        return 0;
+        if (dictionary.contains(word)) {
+            switch (word.length()) {
+            case 0:
+            case 1:
+            case 2:
+                return 0;
+            case 3:
+            case 4:
+                return 1;
+            case 5:
+                return 2;
+            case 6:
+                return 3;
+            case 7:
+                return 5;
+            default:
+                return 11;
+            }
+        } else {
+            return 0;
+        }
     }
 
     public static void main(String[] args)
