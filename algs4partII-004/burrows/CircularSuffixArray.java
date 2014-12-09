e2778d090cd29c67ddd9e8d023406a834fc12f17
vinsonlee <vinsonlee@users.noreply.github.com>
2014-12-09 10:36:07 -0800

    algs4partII-004/burrows: Initial files.
    Compilation:  PASSED
    Style:        PASSED
    Findbugs:     No potential bugs found.
    API:          PASSED
    
    Correctness:  0/23 tests passed
    Memory:       10/10 tests passed
    Timing:       78/78 tests passed
    
    Aggregate score: 35.00% [Correctness: 65%, Memory: 10%, Timing: 25%, Style: 0%]

---
+++ algs4partII-004/burrows/CircularSuffixArray.java
@@ -0,0 +1,19 @@
+public class CircularSuffixArray {
+    // circular suffix array of s
+    public CircularSuffixArray(String s) {
+    }
+
+    // length of s
+    public int length() {
+        return 0;
+    }
+
+    // returns index of ith sorted suffix
+    public int index(int i) {
+        return 0;
+    }
+
+    // unit testing of the methods (optional)
+    public static void main(String[] args) {
+    }
+}
