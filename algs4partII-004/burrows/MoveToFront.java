e8af5427252709f3366b2a6cc21aac8e9941a730
vinsonlee <vinsonlee@users.noreply.github.com>
2014-12-09 17:00:10 -0800

    algs4partII-004/burrows: Implemented MoveToFront::encode.
    Compilation:  PASSED
    Style:        PASSED
    Findbugs:     No potential bugs found.
    API:          PASSED
    
    Correctness:  0/23 tests passed
    Memory:       10/10 tests passed
    Timing:       78/78 tests passed
    
    Aggregate score: 35.00% [Correctness: 65%, Memory: 10%, Timing: 25%, Style: 0%]

--- algs4partII-004/burrows/MoveToFront.java
+++ algs4partII-004/burrows/MoveToFront.java
@@ -1,6 +1,34 @@
 public class MoveToFront {
     // apply move-to-front encoding, reading from standard input and writing to standard output
     public static void encode() {
+        char[] sequence = new char[256];
+        for (int i = 0; i < sequence.length; i++) {
+            sequence[i] = (char) i;
+        }
+
+        // read the input
+        String s = BinaryStdIn.readString();
+        char[] input = s.toCharArray();
+
+        for (int i = 0; i < input.length; i++) {
+            char in = input[i];
+
+            int out;
+            for (out = 0; out < sequence.length; out++) {
+                if (sequence[out] == in) {
+                    break;
+                }
+            }
+            assert out < sequence.length;
+
+            BinaryStdOut.write((char) out);
+
+            System.arraycopy(sequence, 0, sequence, 1, out);
+            sequence[0] = in;
+        }
+
+        // close output stream
+        BinaryStdOut.close();
     }
 
     // apply move-to-front decoding, reading from standard input and writing to standard output
@@ -10,5 +38,8 @@ public class MoveToFront {
     // if args[0] is '-', apply move-to-front encoding
     // if args[0] is '+', apply move-to-front decoding
     public static void main(String[] args) {
+        if      (args[0].equals("-")) encode();
+        else if (args[0].equals("+")) decode();
+        else throw new IllegalArgumentException("Illegal command line argument");
     }
 }
