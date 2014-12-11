19f833ece915d6d79b034ce4324c6c97199c19e6
vinsonlee <vinsonlee@users.noreply.github.com>
2014-12-11 14:25:56 -0800

    algs4partII-004/burrows: WIP
    Using SuffixArrayX passses timing tests.
    Using SuffixArray does not.
    
    Compilation:  PASSED
    Style:        PASSED
    Findbugs:     No potential bugs found.
    API:          PASSED
    
    Correctness:  4/23 tests passed
    Memory:       10/10 tests passed
    Timing:       78/78 tests passed
    
    Aggregate score: 46.30% [Correctness: 65%, Memory: 10%, Timing: 25%, Style: 0%]

--- algs4partII-004/burrows/CircularSuffixArray.java
+++ algs4partII-004/burrows/CircularSuffixArray.java
@@ -1,19 +1,40 @@
 public class CircularSuffixArray {
+    private SuffixArrayX suffix;
+
     // circular suffix array of s
     public CircularSuffixArray(String s) {
+        if (s == null) throw new NullPointerException();
+
+        suffix = new SuffixArrayX(s);
     }
 
     // length of s
     public int length() {
-        return 0;
+        return suffix.length();
     }
 
     // returns index of ith sorted suffix
     public int index(int i) {
-        return 0;
+        return suffix.index(i);
+    }
+
+    public String toString() {
+        return suffix.toString();
     }
 
     // unit testing of the methods (optional)
     public static void main(String[] args) {
+        String[] strings = {
+            "ABRACADABRA!",
+        };
+
+        for (String s : strings) {
+            CircularSuffixArray suffix = new CircularSuffixArray(s);
+            StdOut.println(s);
+            StdOut.println("length: " + suffix.length());
+            StdOut.println("index(0): " + suffix.index(0));
+            StdOut.println(suffix);
+            StdOut.println();
+        }
     }
 }
